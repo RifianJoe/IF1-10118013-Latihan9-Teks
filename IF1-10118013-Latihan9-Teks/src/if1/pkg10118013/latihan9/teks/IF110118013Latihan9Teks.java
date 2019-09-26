09122104d0d5c9af7c3ea08e6a09f379dfa8d890
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:10:52 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan9-Teks/src/if1/pkg10118013/latihan9/teks/IF110118013Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan9.teks;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " +c);
+    }
+    
+}
